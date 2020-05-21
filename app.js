24a17056478233a05e57fba8d0eb1680bb79098e
Deividas <deivis124@gmail.com>
2020-05-21 23:45:41 +0300

    Makes Monster HP bar decrease
    Adds computed propert: widthHealth, which is responsible for rendering  Moster Health bar width

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 new Vue({
   el: "#app",
   data: {
+    width: "40%",
+    color: "red",
     health: 200,
     player: {
       Health: 100,
-      simpleAttack: 5,
+      simpleAttack: 50,
+      specialAtack: function () {
+        return this.simpleAttack + 5;
+      },
     },
     monster: {
-      Health: 200,
+      Health: 100,
+    },
+    log: ["testas", "kaskas"],
+    styleHealthBar: {
+      backgroundColor: "green",
+      margin: 0,
+      // color: "white",
+      width: "50%",
     },
   },
   methods: {
-    attackMonster: function () {
+    attackMonster() {
       this.monster.Health -= this.player.simpleAttack;
       console.log(this.monster.Health);
+      console.log(this.player.specialAtack());
+    },
+  },
+  computed: {
+    widthHealth() {
+      if (this.monster.Health <= 0) {
+        return this.monster.Health + "%";
+      }
+      return this.monster.Health + "%";
     },
   },
 });

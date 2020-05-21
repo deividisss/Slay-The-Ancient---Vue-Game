a19d0abfa61f613177ad3471fd5e3c78e6c504a4
Deividas <deivis124@gmail.com>
2020-05-21 06:32:57 +0300

    Adds method: attackMonster()
    Reduces monster Hp by player simpleAttack value

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 new Vue({
   el: "#app",
   data: {
+    health: 200,
     player: {
       Health: 100,
+      simpleAttack: 5,
     },
     monster: {
       Health: 200,
+    },
+  },
+  methods: {
+    attackMonster: function () {
+      this.monster.Health -= this.player.simpleAttack;
+      console.log(this.monster.Health);
     },
   },
 });

0d97e37dfa0c4645bc6f57604f54d48c4ef2bed9
Deividas <deivis124@gmail.com>
2020-05-29 02:01:11 +0300

    Adds functionality to the Log to Display Player attacks

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ new Vue({
   el: "#app",
   data: {
     stateWon: false,
+    playerTurn: true,
     width: "40%",
     color: "red",
     health: 200,
     player: {
       Health: 100,
-      simpleAttack: 50,
+      simpleAttack: 30,
       specialAtack: function () {
         return this.simpleAttack + 5;
       },
@@ -27,28 +28,38 @@ new Vue({
   methods: {
     attackMonster() {
       this.monster.Health -= this.player.simpleAttack;
-      console.log(this.monster.Health);
+      this.addToLog(this.player.simpleAttack);
+      console.log(this.log);
+
+      console.log(this.player.simpleAttack);
       console.log(this.player.specialAtack());
+    },
+    //gets values and adds the to the array
+    addToLog(argument) {
+      this.log.unshift(argument);
     },
   },
   computed: {
     widthHealth() {
       if (this.monster.Health <= 0) {
+        // alert("what");
         var kaskas = document.getElementById("logas");
         kaskas.style.display = "none";
         this.stateWon = !this.stateWon;
         setTimeout(function () {
           alert("You win!");
-        }, 500);
-
+        }, 550);
+        // alert("You win! after");
+        this.monster.Health = 0;
         return this.monster.Health + "%";
       }
+
       return this.monster.Health + "%";
     },
   },
   watch: {
     stateWon: function () {
-      this.monster.Health = 0;
+      //this.monster.Health = 0;
     },
   },
 });
